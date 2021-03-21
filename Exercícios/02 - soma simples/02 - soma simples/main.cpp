57815529b37d1701f0216782db1ef446563f9110
Danillo <danillodca@gmail.com>
2021-03-20 23:14:47 -0300

    primeiro commit de exercícios

---
+++ Exercícios/02 - soma simples/02 - soma simples/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number1, number2, sum;
+    cout<<"Enter your first number: ";
+    cin>>number1;
+    cout<<"Enter your second number: ";
+    cin>>number2;
+    sum = number1 + number2;
+    cout<<"Sum now is:"<<sum;
+
+    return 0;
+}
